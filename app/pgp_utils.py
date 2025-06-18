9117d7afa2b756b59783ec9b48cf9403cfb8b497
JJFABxIT <142099482+JJFABxIT@users.noreply.github.com>
2025-06-18 22:08:35 +0100

    Update pgp_utils.py
    Add PGP signing and verification

--- app/pgp_utils.py
+++ app/pgp_utils.py
@@ -4,9 +4,26 @@ def load_key(key_str: str):
     key, _ = pgpy.PGPKey.from_blob(key_str)
     return key 
 
-def encrypt_message(pubkey_str: str, message: str):
+def sign_message(privkey_str: str, passphrase: str, message: str):
+    privkey = load_key(privkey_str)
+    pgp_message = pgpy.PGPMessage.new(message)
+    if privkey.is_protected:
+        with privkey.unlock(passphrase):
+            signature = privkey.sign(pgp_message)
+    else:
+        signature = privkey.sign(pgp_message)
+    pgp_message |= signature  # Embed the signature inside the message
+    return str(pgp_message)
+
+def encrypt_message(pubkey_str: str, message: str, signed=False, privkey_str=None, passphrase=None):
     pubkey = load_key(pubkey_str)
-    msg = pgpy.PGPMessage.new(message)
+    
+    if signed and privkey_str:
+        message = sign_message(privkey_str, passphrase, message)
+        msg = pgpy.PGPMessage.from_blob(message)
+    else:
+        msg = pgpy.PGPMessage.new(message)
+
     encrypted = pubkey.encrypt(msg)
     return str(encrypted)
 
@@ -22,4 +39,9 @@ def decrypt_message(privkey_str: str, passphrase: str, encrypted_str: str):
     else:
         message = pgpy.PGPMessage.from_blob(encrypted_str)
         decrypted = privkey.decrypt(message)
-    return str(decrypted.message)
+    return decrypted  # Return the full object so you can verify signature
+
+def verify_signature(pubkey_str: str, decrypted_msg):
+    pubkey = load_key(pubkey_str)
+    verified = pubkey.verify(decrypted_msg)
+    return verified
